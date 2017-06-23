cec0265a76909d094a58aa291cb734907e3a476b
Zintis <Zintis>
2017-06-23 18:11:19 -0400

    still setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { TableBodyComponent } from './table-body/table-body.component';
+import { TableRowComponent } from './table-row/table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    TableBodyComponent,
+    TableRowComponent
   ],
   imports: [
     BrowserModule
